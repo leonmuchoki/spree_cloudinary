dae1dbb15a0b7ce66a4ff4cf4bfa378c49e6544c
LEON MUCHOKI WANGECHI <muchokileon@gmail.com>
2020-04-22 08:23:14 +0300

    create image.rb

---
+++ config/initializers/image.rb
@@ -0,0 +1,15 @@
+require 'spree/core'
+Spree::Image.class_eval do
+  mount_uploader :attachment, CloudinaryUploader, :mount_on => :attachment_file_name
+
+  # Get rid of the paperclip callbacks
+  def save_attached_files; end
+  def prepare_for_destroy; end
+  def destroy_attached_files; end
+
+  private
+  # Get rid of Paperclip validation
+  def attachment_file_name
+    "not_blank"
+  end
+end
